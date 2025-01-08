9594ecf3ae15000eccc7792da4d5c67849a0caf2
sdj <sdj0503123@gmail.com>
2025-01-09 03:36:11 +0900

    scroll-bar 추가

--- src/scoll.js
+++ src/scoll.js
@@ -1,6 +1,7 @@
 const sections = document.querySelectorAll('section');
 const headLinks = document.querySelectorAll('.head a');
 const projectSection = document.querySelector('.project');
+const contactSection = document.querySelector('.send-mail');
 const Section = {
     home: '#home', 
     resume: '#resume',
@@ -23,13 +24,22 @@ const headEffect = (entries) => {
                     projectSection.classList.add('active');
                     // project 섹션에 들어오면 색상 변경
                 });
-           
+                
+
             } else {
                 headLinks.forEach(link => {
                     link.style.color = ''; // 다른 섹션일 때 색상 초기화
                 });
                 projectSection.classList.remove('active');
             }
+            if(entry.target.id === 'mail'){
+                contactSection.classList.add('active');
+            }
+            else{
+                contactSection.classList.remove('active');
+                
+            }
+
         } else {
             link.classList.remove('active');
             link.style.color = ''; // 섹션을 벗어나면 색상 초기화
