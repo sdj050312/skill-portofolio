d79e9318b87ab559b03d637a6761cf3498dd9bf8
sdj050312 <sdj0503123@gmail.com>
2025-01-08 21:24:54 +0900

    profile-update

--- src/dev.js
+++ src/dev.js
@@ -1,22 +1,27 @@
-// const item = document.getElementById('item_hover');
-// const imgHover = document.querySelector('.content_img_title img');
-// const images = [
-//     'education.jpg',
-//     'image.jpg',
-//     'image01.jpg',
-//     'image02.jpg'
-// ];
-// console.log(item);
-// let currentIndex =0; 
-// let intervalId;
-// item.addEventListener('mouseover', ()=>{
-//     intervalId = setInterval(()=>{
-//         imgHover.src = images[currentIndex];
-//         currentIndex = (currentIndex + 1) % images.length;
-//     }, 500)
-// });
-// console.log(item)
-// item.addEventListener('mouseout', () => {
-//     clearInterval(intervalId); // 마우스가 떠나면 이미지 변경 중지
-//     currentIndex = 0; // 인덱스 초기화 (선택 사항)
-// });
+const item = document.getElementById('item_hover');
+const imgHover = document.querySelector('.content_img_title img');
+let images = [
+    './img/education.jpg',
+    './img/image.jpg',
+    './img/image01.jpg',
+    './img/image02.jpg'
+];
+console.log(item);
+let currentIndex = 0; 
+let intervalId;
+
+item.addEventListener('mouseover', () => {
+    item.style.objectFit = 'cover';  
+    imgHover.style.width = '100%';   
+    imgHover.style.height = '100%';  
+
+    intervalId = setInterval(() => {
+        imgHover.src = images[currentIndex];
+        currentIndex = (currentIndex + 1) % images.length;
+    }, 500);
+});
+
+item.addEventListener('mouseout', () => {
+    clearInterval(intervalId);  // Stop the image change when mouse leaves
+    currentIndex = 0; 
+});
